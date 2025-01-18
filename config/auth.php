f9c6d7998043803fa18c12bd5322008855111463
muhamad alfin pratama <muhamadalfinpratamaa@gmail.com>
2025-01-18 13:36:38 +0700

    add footer, header, and sidebar components

--- config/auth.php
+++ config/auth.php
@@ -1,6 +1,1 @@
-<?php
-session_start();
-if (!isset($_SESSION['user']) || !isset($_SESSION['admin'])) {
-    header('Location: ../pages/login.php');
-    exit;
-}
+// config for auth
--- config/db.php
+++ config/db.php
@@ -1,15 +1,1 @@
-<?php
-
-$host = 'localhost';
-$db = 'te_management';
-$user = 'root';
-$pass = '';
-
-try {
-    $pdo = new PDO("mysql:host={$host};dbname={$db}", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    die("Failed to connect to the database: " . $e->getMessage());
-}
+// config for db
